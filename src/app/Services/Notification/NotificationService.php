825949fe709d7cb8d7eafa9ba1960641e88a3d4d
RichardVsc <viniciusrvsc@gmail.com>
2025-04-03 17:59:30 -0300

    feat: use Redis queue

--- src/app/Services/Notification/NotificationService.php
+++ src/app/Services/Notification/NotificationService.php
@@ -19,6 +19,6 @@ class NotificationService
      */
     public function send(int $userId, string $message)
     {
-        SendNotificationJob::dispatch($userId, $message)->onQueue('default');
+        SendNotificationJob::dispatch($userId, $message);
     }
 }
